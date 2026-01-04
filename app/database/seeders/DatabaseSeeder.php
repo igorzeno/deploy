7b023b91b3769c302ce6ebea13929b208a61775a
IgorStarozhuckov <starozhuckov@yandex.ru>
2026-01-04 10:52:04 +0300

    fix commit 2

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -12,18 +12,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-        Travel::factory()
-            ->count(2)
-            ->hasTours(5)
-            ->create();
-        Travel::factory()
-            ->count(3)
-            ->hasTours(6)
-            ->create();
-        Travel::factory()
-            ->count(4)
-            ->hasTours(2)
-            ->create();
+        $this->call([
+            TravelSeederProd::class,
+        ]);
+//
+//
+//            Travel::factory()
+//                ->count(2)
+//                ->hasTours(5)
+//                ->create();
+//        Travel::factory()
+//            ->count(3)
+//            ->hasTours(6)
+//            ->create();
+//        Travel::factory()
+//            ->count(4)
+//            ->hasTours(2)
+//            ->create();
+
     }
 }
